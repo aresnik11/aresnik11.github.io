bda01d4312e4b8cd77a66f8afa11d71bef52b751
Amy Resnik <amyresnik@amys-air.home>
2019-06-08 20:52:45 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// JavaScript Document
+// jQuery function to toggle different sections opened and closed
+// .menu is the class of the divs always shown
+// #tab is the id of the divs to show when toggled
 
 function main() {
 		$(".menu").click(function() {
